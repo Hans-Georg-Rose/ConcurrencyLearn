b98f5a354d8ba98fe0ce999e3980bac43e02cc9a
Hans-Georg Rose <hansgeorg.rose@picobyte.de>
2022-03-31 18:37:26 +0200

    Initial Commit

---
+++ MacOS Concurrency/MacOS_ConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacOS_ConcurrencyApp.swift
+//  MacOS Concurrency
+//
+//  Created by Hans-Georg Rose on 31.03.22.
+//
+
+import SwiftUI
+
+@main
+struct MacOS_ConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
